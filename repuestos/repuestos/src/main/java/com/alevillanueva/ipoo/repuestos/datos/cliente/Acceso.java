8ab575152b83a8912bcb75209ab503cbf748439d
alumno-202 <alumno-202@M202-12.uaa.local>
2017-05-03 21:44:54 -0400

    Creación de clases acceso

---
+++ repuestos/repuestos/src/main/java/com/alevillanueva/ipoo/repuestos/datos/cliente/Acceso.java
@@ -0,0 +1,19 @@
+package com.alevillanueva.ipoo.repuestos.datos.cliente;
+
+import java.util.Date;
+
+public class Acceso {
+//atributos
+	private Date fechaHoraInicio;
+	private Date fechaHoraFin;
+	private String ip;
+	private Cliente Cliente;
+
+	
+//Métodos
+	public Acceso(){
+
+
+}
+	
+}//fin de la clase
